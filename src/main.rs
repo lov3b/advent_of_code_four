55bd0c29507c5e2ed2fadbb7cc3a3920c036a639
lov3b <lovebillenius@protonmail.com>
2021-12-10 18:06:14 +0100

    duno what to say honestly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-fn main() {}
+use std::fs;
+fn main() {
+    let input_common = fs::read_to_string("input_common.txt").unwrap();
+    #[allow(unused)]
+    let input = fs::read_to_string("input.txt").unwrap();
 
+    println!("{}", &input_common);
+
+    println!("Board 1");
+
+    let mut lines = input_common.lines();
+    let generated = lines.nth(0).unwrap();
+    let lines = lines.skip(1);
+    println!("genrated: {}", &generated);
+
+    println!("\nRest");
+    for i in lines {
+        println!("{}", &i);
+    }
+}
+
+#[allow(unused)]
 struct Board {
     line_1: Vec<u32>,
     line_2: Vec<u32>,
@@ -8,8 +28,15 @@ struct Board {
     line_5: Vec<u32>,
 }
 
-impl Board{
-    fn from(board_str: String) -> Self{
-
+impl Board {
+    #[allow(unused)]
+    fn from(board_str: String) -> Self {
+        Self {
+            line_1: Vec::new(),
+            line_2: Vec::new(),
+            line_3: Vec::new(),
+            line_4: Vec::new(),
+            line_5: Vec::new(),
+        }
     }
 }
