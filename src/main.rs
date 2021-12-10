5b58433cb2bbe26045b1c654a03b2e38caab5480
lov3b <lovebillenius@protonmail.com>
2021-12-10 17:53:33 +0100

    Initial (I unfortinatly deleted my previuos solution)

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {}
+
+struct Board {
+    line_1: Vec<u32>,
+    line_2: Vec<u32>,
+    line_3: Vec<u32>,
+    line_4: Vec<u32>,
+    line_5: Vec<u32>,
+}
+
+impl Board{
+    fn from(board_str: String) -> Self{
+
+    }
+}
